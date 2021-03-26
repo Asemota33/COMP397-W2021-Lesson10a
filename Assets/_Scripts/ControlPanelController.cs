8d4397eb090dadb57fe3bd12329904ce6f073b59
Michael Asemota <michaelasemota1@hotmail.com>
2021-03-26 17:00:59 -0400

    Fixed load and save

--- Assets/_Scripts/ControlPanelController.cs
+++ Assets/_Scripts/ControlPanelController.cs
@@ -26,6 +26,11 @@ public class ControlPanelController : MonoBehaviour
 
     public GameObject gameStateElement;
 
+    void Awake()
+    {
+       
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +40,16 @@ public class ControlPanelController : MonoBehaviour
         rectTransform = GetComponent<RectTransform>();
         rectTransform.anchoredPosition = offScreenPosition;
         timer = 0.0f;
+
+        //var sceneDataJSONString = PlayerPrefs.GetString("playerData");
+        //JsonUtility.FromJsonOverwrite(sceneDataJSONString, sceneData);
+        //Deserializing and loading data from player preferences
+        //sceneData = JsonUtility.FromJson<SceneDataSO>(sceneDataJSONString);
+
+
+        LoadFromPlayerPrefs();
+
+
     }
 
     // Update is called once per frame
@@ -107,8 +122,10 @@ public class ControlPanelController : MonoBehaviour
 
     public void OnLoadButtonPressed()
     {
+        LoadFromPlayerPrefs();
         player.controller.enabled = false;
         player.transform.position = sceneData.playerPosition;
+        player.transform.rotation = sceneData.playerRotation;
         player.controller.enabled = true;
 
         player.health = sceneData.playerHealth;
@@ -119,5 +136,39 @@ public class ControlPanelController : MonoBehaviour
     {
         sceneData.playerPosition = player.transform.position;
         sceneData.playerHealth = player.health;
+        sceneData.playerRotation = player.transform.rotation;
+
+        //serialize and save our data to Player prefences to localdb/dictionary
+        //PlayerPrefs.SetString("playerData", JsonUtility.ToJson(sceneData));
+        SavetoPlayerPrefs();
+    }
+
+    public void SavetoPlayerPrefs()
+    {
+        PlayerPrefs.SetFloat("playerTransformX", sceneData.playerPosition.x);
+        PlayerPrefs.SetFloat("playerTransformY", sceneData.playerPosition.y);
+        PlayerPrefs.SetFloat("playerTransformZ", sceneData.playerPosition.z);
+
+        PlayerPrefs.SetFloat("playerRotatationZ", sceneData.playerRotation.z);
+        PlayerPrefs.SetFloat("playerRotatationY", sceneData.playerRotation.y);
+        PlayerPrefs.SetFloat("playerRotatationX", sceneData.playerRotation.x);
+        PlayerPrefs.SetFloat("playerRotatationW", sceneData.playerRotation.w);
+
+        PlayerPrefs.SetInt("playerHealth", sceneData.playerHealth);
+    }
+
+    public void LoadFromPlayerPrefs()
+    {
+        sceneData.playerPosition.x = PlayerPrefs.GetFloat("playerTransformX");
+        sceneData.playerPosition.y = PlayerPrefs.GetFloat("playerTransformY");
+        sceneData.playerPosition.z = PlayerPrefs.GetFloat("playerTransformZ");
+
+        sceneData.playerRotation.z = PlayerPrefs.GetFloat("playerRotatationZ");
+        sceneData.playerRotation.y = PlayerPrefs.GetFloat("playerRotatationY");
+        sceneData.playerRotation.x = PlayerPrefs.GetFloat("playerRotatationX");
+        sceneData.playerRotation.w = PlayerPrefs.GetFloat("playerRotatationW");
+
+        sceneData.playerHealth = PlayerPrefs.GetInt("playerHealth");
+
     }
 }
